a4019be856c69b8e8e2dfd6f595d683ab1c0a40a
Joel <johnson.joel.86@gmail.com>
2013-01-10 14:40:25 -0700

    making the scripts editable

--- src/main/java/com/attask/jenkins/ScriptApi.java
+++ src/main/java/com/attask/jenkins/ScriptApi.java
@@ -2,15 +2,18 @@ package com.attask.jenkins;
 
 import hudson.Extension;
 import hudson.model.RootAction;
+import hudson.model.User;
+import hudson.security.AccessDeniedException2;
+import hudson.security.Permission;
+import jenkins.model.Jenkins;
 import org.apache.commons.io.FileUtils;
 import org.apache.log4j.lf5.util.StreamUtils;
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
+import org.kohsuke.stapler.interceptor.RequirePOST;
 
 import javax.servlet.ServletOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * User: Joel Johnson
@@ -20,6 +23,14 @@ import java.io.IOException;
 @Extension
 public class ScriptApi implements RootAction {
 	public void doFile(StaplerRequest request, StaplerResponse response) throws IOException {
+		if(!Jenkins.getAuthentication().isAuthenticated()) {
+			throw new AccessDeniedException2(Jenkins.getAuthentication(), Permission.CONFIGURE);
+		}
+		User current = User.current();
+		if(current != null) {
+			current.checkPermission(Permission.CONFIGURE);
+		}
+
 		String path = request.getParameter("path");
 		ServletOutputStream outputStream = response.getOutputStream();
 		FileInputStream input = new FileInputStream(new File(path));
@@ -29,6 +40,22 @@ public class ScriptApi implements RootAction {
 			input.close();
 		}
 		outputStream.flush();
+	}
+
+	@RequirePOST
+	public void doUpdateFile(StaplerRequest request, StaplerResponse response) throws IOException {
+		if(!Jenkins.getAuthentication().isAuthenticated()) {
+			throw new AccessDeniedException2(Jenkins.getAuthentication(), Permission.CONFIGURE);
+		}
+		User current = User.current();
+		if(current != null) {
+			current.checkPermission(Permission.CONFIGURE);
+		}
+
+		String path = request.getParameter("path");
+		String content = request.getParameter("content");
+
+		FileUtils.writeStringToFile(new File(path), content);
 	}
 
 	public String getIconFileName() {
---
+++ src/main/webapp/scripts/scriptViewer.js
@@ -0,0 +1,71 @@
+function hideContent(guid) {
+	var codeTag = $('codeEditor-' + guid);
+	codeTag.style.display = "none";
+}
+
+function showContent(guid) {
+	var selectBox = $('select-' + guid);
+
+	var url = ROOT_URL;
+	if(!url.endsWith('/')) {
+		url = url + '/';
+	}
+
+	new Ajax.Request(url+'scriptApi/file', {
+		method: 'get',
+		parameters: { 'path': selectBox.value },
+		evalJS: 'false',
+		onSuccess: function(transport) {
+			var codeTag = $('code-' + guid);
+			codeTag.innerText = transport.responseText;
+
+			var codeEditor = $('codeEditor-' + guid);
+			codeEditor.style.display = "block";
+			prettyPrint();
+		},
+		onError: function(transport) {
+			console.log("failure!", transport);
+		}
+	});
+}
+
+function selectChanged(guid) {
+	var codeTag = $('codeEditor-' + guid);
+	if(codeTag.style.display == "block") {
+		showContent(guid);
+	}
+}
+
+function loadPreview(guid) {
+	var codeTag = $('codeEditor-' + guid);
+	if(codeTag.style.display == "block") {
+		hideContent(guid);
+	} else {
+		showContent(guid);
+	}
+
+	return false;
+}
+
+function saveChanges(guid) {
+	var url = ROOT_URL;
+	if(!url.endsWith('/')) {
+		url = url + '/';
+	}
+
+	var selectBox = $('select-' + guid);
+	var codeEditor = $('codeEditor-' + guid);
+
+	new Ajax.Request(url+'scriptApi/updateFile', {
+		method: 'post',
+		parameters: { 'path': selectBox.value, 'content': codeEditor.innerText },
+		evalJS: 'false',
+		onSuccess: function(transport) {
+			//Nothing to do here, move along
+		},
+		onError: function(transport) {
+			alert("failed to save!", transport);
+			console.log("failed to save!");
+		}
+	});
+}
